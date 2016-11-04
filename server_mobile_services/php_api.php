8c6c3e2bf22e2f087f3dda5ff46bb0020005e345
wojtekiexample <wojciechsajan@gmail.com>
2016-11-04 23:41:39 +0100

    commit

---
+++ server_mobile_services/php_api.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+/*
+
+function send_funny_content($user OR $trendsetter, chain1 position, chain2 position){
+
+}
+
+function react_to_content($objectID, $reaction){
+
+}
+
+
+
+
+**/
+
+
+
+?> 
+
