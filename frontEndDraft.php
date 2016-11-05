d666b84da658162fef95ef1996a66e584a45ca5e
sajan <sajanmaciej@gmail.com>
2016-11-05 18:45:24 +0100

    et

---
+++ frontEndDraft.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>front</title>
+</head>
+<script type="text/javascript" src="/js/SajanaJsonAjax.js" ></script>
+
+
+dodaj usera:
+<form id="dadajUseraForm">
+	login:<input id="dodajUseraLoginInput" type="text" /><br/>
+	password:<input id="dodajUseraPasswordInput" type="password" /><br/>
+    e-mail:<input id="dodajUseraEMailInput" type="email" /><br/>
+</form>
+<div id="dodajUseraBtn">Dodaj</div>
+
+
+
+
+<script type="text/javascript" src="js/frontEndDraft.js"></script>
+
+
+<body>
+</body>
+</html>
