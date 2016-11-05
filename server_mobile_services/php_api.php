a73a05136e18b198944905e60533720134042d91
sajan <sajanmaciej@gmail.com>
2016-11-05 03:06:37 +0100

    php API change

--- server_mobile_services/php_api.php
+++ server_mobile_services/php_api.php
@@ -1,6 +1,6 @@
 <?php
 //laczymy z bazka
-@ $bazaPortfolio = new mysqli('mariadb5.iq.pl','iexample_fnm','rcdky4y3zu','iexample_fnm');
+@ $baza = new mysqli('mariadb5.iq.pl','iexample_fnm','rcdky4y3zu','iexample_fnm');
 if (mysqli_connect_errno()){
 echo 'Wystąpił bląd nie udało się połączyć z bazą. ';	
 }
@@ -20,13 +20,32 @@ function react_to_content($objectID, $reaction){
 **/
 
 
-
-
-
-function addUser($userName,$password,$eMail){
-	
-
+if (isset($_GET['action'])){
+	/*###### 	FUNKCJA  addUser	 #######*/
+	if ($_GET['action'] == 'addUser'){
+		if (isset($GET('login')) && isset($GET('password'))&& isset($GET('eMail'))){
+			$login = $GET('login');
+			$password = $GET('password');
+			$passordHash = md5($password);
+			$eMail = $GET('eMail');
+			
+			$sqlQueryString = "INSERT INTO `users` set `login`='$login', `passwordHash` = '$passwordHash',`eMail`='$eMail'";
+			if (!$baza->query("$sqlQuery")){
+				echo('coś sie obsrało z wysłaniem do bazy');
+			}else{
+				echo('gites powinno być w bazie');
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
 
 
 
